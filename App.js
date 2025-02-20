c9001af2c090779a7676c1a3fd206b550de17032
Sonu7788 <sonujangir992002@gmail.com>
2025-02-20 06:15:42 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+
+function App() {
+  let [pstatus,setpstatus]=useState(false);
+  let [mstatus, setmstatus] = useState(false);
+  // alert("HI")
+  return (
+    <div>
+      <button className='micon' onClick={()=>setmstatus(!mstatus)}>
+          {mstatus ? 
+          <span>&times;</span>
+        :
+        <span>&#9776;</span>}
+        </button>
+      <div className={`menu ${mstatus ? 'activemenu':''}`}>
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>course</li>
+          <li>Gallery</li>
+        </ul>
+      </div>
+      
+      <input type={pstatus ? 'text' :'password'}></input>
+      <button onClick={()=>setpstatus(!pstatus)}>{pstatus? 'hide':'show'}</button>
+    </div>
+  );
+}
+
+export default App;
